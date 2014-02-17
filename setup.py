fa2bc1a6d90f5d241cab2316506b21c740cd4977
Martijn Meijers <b.m.meijers@tudelft.nl>
2014-02-17 15:55:21 +0100

    Refactoring setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
-import sys
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages, Extension
 import os
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "fake_pyrex"))
-from Cython.Distutils import build_ext
-from setuptools import setup, Extension
-
-# TODO: Read http://infinitemonkeycorps.net/docs/pph/
-# it has useful tips on getting a package out in a versioned way
+import sys
+try:
+    from Cython.Build import cythonize
+    cython_available = True
+except ImportError:
+    cython_available = False
 
 def get_version():
     """
     Gets the version number. Pulls it from the source files rather than
     duplicating it.
-    
     """
-    # we read the file instead of importing it as root sometimes does not
-    # have the cwd as part of the PYTHONPATH
     fn = os.path.join(os.path.dirname(__file__), 'src', 'simplegeom', '__init__.py')
     try:
         lines = open(fn, 'r').readlines()
@@ -33,36 +31,43 @@ def get_version():
                            "'__version__ =' string not found")
     return version
 
+PACKAGES = find_packages('src')
+if cython_available:
+    EXT_MODULES = cythonize(["src/simplegeom/_geom2d.pyx",
+                                "src/simplegeom/_wkb.pyx",
+                                "src/simplegeom/_wkt.pyx",
+                                "src/simplegeom/_util.pyx",])
+else:
+    sys.stderr.write("Cython NOT available, building from .C sources\n")
+    EXT_MODULES = [
+        Extension('simplegeom._geom2d', 
+                  ['src/simplegeom/_geom2d.c']),
+        Extension('simplegeom._wkb', 
+                  ['src/simplegeom/_wkb.c']),
+        Extension('simplegeom._wkt', 
+                  ['src/simplegeom/_wkt.c']),
+        Extension('simplegeom._util', 
+                  ['src/simplegeom/_util.c']),
+    ]
+SCRIPTS = [] 
+REQUIREMENTS = []
+DATA_FILES = []
 
 setup(
     name = "simplegeom",
     version = get_version(),
+    packages = PACKAGES,
+    package_dir = {"": "src"},
     author = "Martijn Meijers",
-    author_email = "b dot m dot meijers at tudelft dot nl",
-    license = "MIT license",
+    author_email = "b.m.meijers@tudelft.nl",
     description = "Simple Feature Geometry objects for Python",
     url = "https://bitbucket.org/bmmeijers/simplegeom/",
-    package_dir = {'':'src'},
-    cmdclass = {'build_ext': build_ext},
-    packages=['simplegeom'], 
-    ext_modules = [
-        Extension("simplegeom._geom2d", 
-            sources = ["src/simplegeom/_geom2d.pyx",],
-            extra_compile_args=[],
-            extra_link_args=[],),
-        Extension("simplegeom._wkb", 
-            sources = ["src/simplegeom/_wkb.pyx",],
-            extra_compile_args=[],
-            extra_link_args=[],),
-        Extension("simplegeom._wkt", 
-            sources = ["src/simplegeom/_wkt.pyx",],
-            extra_compile_args=[],
-            extra_link_args=[],),
-        Extension("simplegeom._util", 
-            sources = ["src/simplegeom/_util.pyx",],
-            extra_compile_args=[],
-            extra_link_args=[],),
-    ],
+    license = "MIT license",
+    ext_modules = EXT_MODULES,
+    data_files = DATA_FILES,
+    zip_safe = False,
+    scripts = SCRIPTS,
+    install_requires = REQUIREMENTS,
     classifiers = [
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -71,7 +76,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Cython",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
